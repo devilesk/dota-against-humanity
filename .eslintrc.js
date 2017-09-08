c267130eb923ec8b79c411d33173ebf84811ed3b
devilesk <devilesk@gmail.com>
2017-09-08 11:19:45 -0400

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            { "SwitchCase": 1 }
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [
+            "error",
+            { "args": "none" }
+        ]
+    },
+    "globals": {
+        "$": true,
+        "Players": true,
+        "Game": true,
+        "GameUI": true,
+        "GameEvents": true,
+        "DOTATeam_t": true,
+        "DotaDefaultUIElement_t": true,
+        "DOTA_GameState": true,
+        "CustomNetTables": true
+    }
+};
